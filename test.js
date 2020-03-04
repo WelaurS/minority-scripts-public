9d87e156701218946889c193e1ebb4bb060a41f7
Sergej Kajzer <welaurs@icloud.com>
2020-03-04 20:57:27 +0300

    initial commit

---
+++ test.js
@@ -0,0 +1,14 @@
+let script = {};
+
+script.OnScriptLoad = () => {
+    console.log(`--> Test script loaded`);
+};
+
+script.OnDraw = () => {
+    Renderer.SetDrawColor(0, 0, 0, 255);
+    Renderer.DrawFilledRect(0, 0, 100, 100);
+};
+
+script.OnScriptUnload = () => {
+    console.log(`--> Test script unloaded`);
+};
